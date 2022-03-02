ee9d8454e34198b8a51b7fa35978bd32d667acea
Veeti Karttunen <veeti.karttunen@aitiofinland.com>
2022-03-02 15:31:09 +0200

    Fix a typo

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -3,11 +3,11 @@ import React from "react";
 import "./Tile.css";
 
 const Tile = (props) => {
-  const colourState =
-    props.selected || props.matched ? { backgroundColour: props.color } : null;
+  const dynamicColor =
+    props.selected || props.matched ? { backgroundColor: props.color } : null;
 
   return (
-    <div className="Tile" style={colourState}>
+    <div className="Tile" style={dynamicColor}>
       {props.selected || props.matched ? <props.svg /> : null}
     </div>
   );
