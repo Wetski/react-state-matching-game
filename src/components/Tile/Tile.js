e390b446e347e2f2ad4f3262cdf6463d6b1f86c2
Veeti Karttunen <veeti.karttunen@aitiofinland.com>
2022-03-02 13:50:54 +0200

    Hydrate the state

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
 
-import './Tile.css'
+import "./Tile.css";
 
-const Tile = () => {
+const Tile = (props) => {
+  const colourState =
+    props.selected || props.matched ? { backgroundColour: props.color } : null;
 
   return (
-    <div className='Tile'>
+    <div className="Tile" style={colourState}>
+      {props.selected || props.matched ? props.svg : null}
     </div>
-  )
-}
+  );
+};
 
-export default Tile
+export default Tile;
